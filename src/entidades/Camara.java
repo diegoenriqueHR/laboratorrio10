f482684f38044245f32f92d735a95ce27b1947f9
diegoenriqueHR <129536078+diegoenriqueHR@users.noreply.github.com>
2023-05-28 23:23:51 -0500

    Add files via upload

---
+++ src/entidades/Camara.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author DIEGO
+ */
+public interface Camara {
+    
+   
+    public void guardarFoto();
+    public void grabarVideo();
+    public void getApertura();
+    
+    
+}
---
+++ src/entidades/Celular.java
@@ -0,0 +1,49 @@
+
+package entidades;
+
+import javax.swing.JOptionPane;
+
+public class Celular 
+        implements Camara, Reproductor, Navegador{
+    
+    
+    
+    public void mostrarMensaje(String mensaje){
+        
+        
+    }
+
+    @Override
+    public void guardarFoto() {
+    }
+
+    @Override
+    public void grabarVideo() {
+    }
+
+    @Override
+    public void getApertura() {
+    }
+
+    @Override
+    public void reproducir() {
+    }
+
+    @Override
+    public void adelantar() {
+    }
+
+    @Override
+    public void retroceder() {
+    }
+
+    @Override
+    public void navegar() {
+    }
+
+    @Override
+    public void abrirLink() {
+    }
+    
+    
+}
